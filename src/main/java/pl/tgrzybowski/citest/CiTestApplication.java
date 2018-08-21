56577b4ce2a6be97c6f220deba4e85e24aee3346
Tomasz Grzybowski <tomasz.grzybowski@student.wat.edu.pl>
2018-08-22 00:44:11 +0300

    init

---
+++ src/main/java/pl/tgrzybowski/citest/CiTestApplication.java
@@ -0,0 +1,12 @@
+package pl.tgrzybowski.citest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiTestApplication.class, args);
+	}
+}
